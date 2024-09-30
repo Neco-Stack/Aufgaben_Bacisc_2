8758bd33bf0fe0e453b88a13c31b84f23902d07d
Neco_Stack <necoabay@googlemail.com>
2024-09-30 18:23:52 +0200

    zufall

--- src/numbers_functions_if/conditionals-ts-level-2_4.ts
+++ src/numbers_functions_if/conditionals-ts-level-2_4.ts
@@ -1,10 +1,11 @@
 // Math.random generiert eine Zufallszahl 
-// Math.floor nennt gerade Zahlen ohne Dezimalstellen
+// Math.floor nennt gerade Zahlen ohne Dezimalstellen, ergo Zahlen ab >1 
 // x77 ergibt eine Zufallszahl zwischen 1 (Math.floor) und 77
 let x : number = Math.floor(Math.random() * 77);
 let y : number = Math.floor(Math.random() *77);
 
 
+// Deklarierung einer Funktion, die zwei Zufallszahlen genereriert- mit Nachricht je nach Ergebnis in Konsole
 function randomLotto(x : number, y : number){
     if (x == y){
         console.log("was für zufall");
@@ -15,6 +16,7 @@ function randomLotto(x : number, y : number){
     }
 }
 randomLotto(x,y)
+
 
 
 
